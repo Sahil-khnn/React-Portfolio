3ea26a1512844826fe6cc2100005d52ae1fadb94
Sahil-khnn <sahil.khn338@gmail.com>
2023-07-12 11:17:55 +0500

    First commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "A Blog Website (MERN-Stack)",
+    description:
+      "A Blog App built in MERN-Stack, With realtime fetching of articles from dev.to using API.",
+    tags: ["React", "Node.Js", "Express Js", "MongoDB"],
+
+    demo: "https://proud-windbreaker-elk.cyclic.app/",
+    github: "https://github.com/Sahil-khnn/Blog-site",
+  },
+  {
+    id: 2,
+    name: "Real Estate website",
+    description:
+      "A Real Estate website build in ReactJS, SASS and framer motion.",
+    tags: ["react", "sass", "framer-motion"],
+
+    demo: "https://real-esttate.netlify.app/",
+    github: "https://github.com/Sahil-khnn/Real-Estate",
+  },
+  {
+    id: 3,
+    name: "Fitness Website (React Clone)",
+    description: "A clone of EvoGym build using ReactJS and SaSS.",
+    tags: ["react", "sass"],
+
+    demo: "https://evogyym.netlify.app/",
+    github: "https://github.com/Sahil-khnn/Evogym",
+  },
+  {
+    id: 4,
+    name: "Todo List (React + Redux)",
+    description:
+      "An advanced todo app build using React,  Redux-toolkit and framer-motion.",
+    tags: ["react", "styledComponents", "react-router"],
+
+    demo: "https://todo-lisstt.netlify.app/",
+    github: "https://github.com/Sahil-khnn/todoList",
+  },
+  {
+    id: 5,
+    name: "Expense Manager App (MERN Stack)",
+    description:
+      "A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    tags: ["React", "Node.Js", "Express Js", "MongoDB"],
+
+    demo: "",
+    github: "",
+  },
+  {
+    id: 6,
+    name: "A Blog Website (MERN-Stack)",
+    description:
+      "A Blog App built in MERN-Stack, With realtime fetching of articles from dev.to using API.",
+    tags: ["React", "Node.Js", "Express Js", "MongoDB"],
+
+    demo: "https://proud-windbreaker-elk.cyclic.app/",
+    github: "https://github.com/Sahil-khnn/Blog-site",
+  },
+];
